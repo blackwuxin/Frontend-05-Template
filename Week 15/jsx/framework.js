412c30ea525f20746cbcda1697ef03fe9043dd2b
wu_xx <wu_xx@ctrip.com>
2021-01-17 19:23:38 +0800

    add

---
+++ Week 15/jsx/framework.js
@@ -0,0 +1,45 @@
+export function createElement(type,attributes,...children){
+    let element = "";
+    if(typeof type == 'string'){
+        element = new ElementWrap(type);
+    }else{
+        element = new type;
+    }
+
+    for(let name in attributes){
+        element.setAttribute(name,attributes[name]);
+    }
+    for(let child of children){
+        if(typeof child == 'string'){
+            child = new TextWrap(child);
+        }
+        child && element.appendChild(child);
+    }
+    return element;
+}
+
+
+export class Component{
+    constructor(){
+
+    }
+    setAttribute(name,value){
+        this.root.setAttribute(name,value);
+    }
+    appendChild(child){
+       child.mountTo(this.root);
+    }
+    mountTo(parent){
+        parent.appendChild(this.render());
+    }
+}
+class ElementWrap extends Component{
+    constructor(type){
+        this.root = document.createElement(type);
+    }
+}
+class TextWrap extends Component{
+    constructor(content){
+        this.root = document.createTextNode(content);
+    }
+}
