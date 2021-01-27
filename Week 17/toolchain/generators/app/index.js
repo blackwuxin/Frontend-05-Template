806727aaaf9d6c25f49b98259625edfda6780c5c
wu_xx <wu_xx@ctrip.com>
2021-01-27 13:55:22 +0800

    update

--- Week 17/toolchain/generators/app/index.js
+++ Week 17/toolchain/generators/app/index.js
@@ -6,30 +6,86 @@ module.exports = class extends Generator {
 
         const answers = await this.prompt([
             {
-              type: "input",
-              name: "name",
-              message: "Your project name",
-              default: this.appname // Default to current folder name
+                type: "input",
+                name: "name",
+                message: "Your project name",
+                default: this.appname // Default to current folder name
             }
-          ]);
-      
+        ]);
+
         this.log("app name", answers.name);
         this.fs.copyTpl(
+            this.templatePath('main.js'),
+            this.destinationPath('src/main.js')
+        );
+        this.fs.copyTpl(
+            this.templatePath('App.vue'),
+            this.destinationPath('src/App.vue')
+        );
+        this.fs.copyTpl(
+            this.templatePath('components/HelloWorld.vue'),
+            this.destinationPath('src/components/HelloWorld.vue')
+        );
+        this.fs.copyTpl(
+            this.templatePath('babel.config.js'),
+            this.destinationPath('babel.config.js')
+        );
+        this.fs.copyTpl(
+            this.templatePath('favicon.ico'),
+            this.destinationPath('public/favicon.ico')
+        );
+        this.fs.copy(
             this.templatePath('index.html'),
-            this.destinationPath('public/index.html'),
-            { title: answers.name }
-          );
-          const pkgJson = {
-            devDependencies: {
-              eslint: '^3.15.0'
+            this.destinationPath('public/index.html')
+        );
+        this.fs.copyTpl(
+            this.templatePath('assets/logo.png'),
+            this.destinationPath('src/assets/logo.png')
+        );
+        const pkgJson =
+        {
+            "name": answers.name,
+            "version": "0.1.0",
+            "private": true,
+            "scripts": {
+                "serve": "vue-cli-service serve",
+                "build": "vue-cli-service build",
+                "lint": "vue-cli-service lint"
             },
-            dependencies: {
-              react: '^16.2.0'
-            }
-          };
-      
-          // Extend or create package.json file in destination path
+            "dependencies": {
+                "core-js": "^3.8.3",
+                "vue": "^2.6.12"
+            },
+            "devDependencies": {
+                "@vue/cli-plugin-babel": "^4.5.11",
+                "@vue/cli-plugin-eslint": "^4.5.11",
+                "@vue/cli-service": "^4.5.11",
+                "babel-eslint": "^10.1.0",
+                "eslint": "^6.8.0",
+                "eslint-plugin-vue": "^6.2.2",
+                "vue-template-compiler": "^2.6.12"
+            },
+            "eslintConfig": {
+                "root": true,
+                "env": {
+                    "node": true
+                },
+                "extends": [
+                    "plugin:vue/essential",
+                    "eslint:recommended"
+                ],
+                "parserOptions": {
+                    "parser": "babel-eslint"
+                },
+                "rules": {}
+            },
+            "browserslist": [
+                "> 1%",
+                "last 2 versions",
+                "not dead"
+            ]
+        };
         this.fs.extendJSON(this.destinationPath('package.json'), pkgJson);
-        this.npmInstall(['lodash'], { 'save-dev': true });
+        this.npmInstall();
     }
 };
--- Week 17/toolchain/generators/app/templates/main.js
+++ Week 17/toolchain/generators/app/templates/main.js
@@ -1,3 +1,8 @@
-for(let i of [1,2,3]){
-    console.log(i);
-}
+import Vue from 'vue'
+import App from './App.vue'
+
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
